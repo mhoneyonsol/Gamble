8ed3f32fa3dec22d911566682c3a15bd5af3517b
mhoneyonsol <166422969+mhoneyonsol@users.noreply.github.com>
2024-06-15 14:50:29 +0200

    Update Header.tsx

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -87,7 +87,7 @@ export default function Header() {
       <StyledHeader>
         <div style={{ display: 'flex', gap: '20px', alignItems: 'center' }}>
           <Logo to="/">
-            <img alt="Gamba logo" src="/logo.svg" />
+            <img alt="Gamba logo" src="https://i.imgur.com/9sHzLo8.png" />
           </Logo>
         </div>
         <div style={{ display: 'flex', gap: '10px', alignItems: 'center', position: 'relative' }}>
